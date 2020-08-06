6784ebb5261ad0d66ea68100661af7af9b951176
Wade-Roberts <45888876+Shadzykin@users.noreply.github.com>
2020-08-06 14:33:19 +0800

    Project Base Completion

--- PomadoroTimer/PomadoroTimerForm.Designer.cs
+++ PomadoroTimer/PomadoroTimerForm.Designer.cs
@@ -30,36 +30,24 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PomadoroForm));
             this.BtnStartBreak = new System.Windows.Forms.Button();
-            this.BtnPauseBreak = new System.Windows.Forms.Button();
             this.TbBreakTime = new System.Windows.Forms.TextBox();
             this.BtnStartPom = new System.Windows.Forms.Button();
-            this.BtnPausePom = new System.Windows.Forms.Button();
             this.TbPomTime = new System.Windows.Forms.TextBox();
-            this.LblBreakTimer = new System.Windows.Forms.Label();
-            this.LblPomTimer = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // BtnStartBreak
             // 
-            this.BtnStartBreak.Location = new System.Drawing.Point(12, 192);
+            this.BtnStartBreak.Location = new System.Drawing.Point(75, 156);
             this.BtnStartBreak.Name = "BtnStartBreak";
-            this.BtnStartBreak.Size = new System.Drawing.Size(75, 23);
+            this.BtnStartBreak.Size = new System.Drawing.Size(100, 50);
             this.BtnStartBreak.TabIndex = 1;
-            this.BtnStartBreak.Text = "Start";
+            this.BtnStartBreak.Text = "Start Break";
             this.BtnStartBreak.UseVisualStyleBackColor = true;
-            // 
-            // BtnPauseBreak
-            // 
-            this.BtnPauseBreak.Location = new System.Drawing.Point(161, 192);
-            this.BtnPauseBreak.Name = "BtnPauseBreak";
-            this.BtnPauseBreak.Size = new System.Drawing.Size(75, 23);
-            this.BtnPauseBreak.TabIndex = 3;
-            this.BtnPauseBreak.Text = "Pause";
-            this.BtnPauseBreak.UseVisualStyleBackColor = true;
+            this.BtnStartBreak.Click += new System.EventHandler(this.BtnStartBreak_Click);
             // 
             // TbBreakTime
             // 
-            this.TbBreakTime.Location = new System.Drawing.Point(75, 163);
+            this.TbBreakTime.Location = new System.Drawing.Point(75, 127);
             this.TbBreakTime.Name = "TbBreakTime";
             this.TbBreakTime.ReadOnly = true;
             this.TbBreakTime.Size = new System.Drawing.Size(100, 23);
@@ -68,65 +56,40 @@
             // 
             // BtnStartPom
             // 
-            this.BtnStartPom.Location = new System.Drawing.Point(12, 56);
+            this.BtnStartPom.Location = new System.Drawing.Point(75, 56);
             this.BtnStartPom.Name = "BtnStartPom";
-            this.BtnStartPom.Size = new System.Drawing.Size(75, 23);
+            this.BtnStartPom.Size = new System.Drawing.Size(100, 50);
             this.BtnStartPom.TabIndex = 1;
-            this.BtnStartPom.Text = "Start";
+            this.BtnStartPom.Text = "Start Pomadoro";
             this.BtnStartPom.UseVisualStyleBackColor = true;
             this.BtnStartPom.Click += new System.EventHandler(this.BtnStartPom_Click);
-            // 
-            // BtnPausePom
-            // 
-            this.BtnPausePom.Location = new System.Drawing.Point(161, 56);
-            this.BtnPausePom.Name = "BtnPausePom";
-            this.BtnPausePom.Size = new System.Drawing.Size(75, 23);
-            this.BtnPausePom.TabIndex = 3;
-            this.BtnPausePom.Text = "Pause";
-            this.BtnPausePom.UseVisualStyleBackColor = true;
             // 
             // TbPomTime
             // 
             this.TbPomTime.Location = new System.Drawing.Point(75, 27);
             this.TbPomTime.Name = "TbPomTime";
+            this.TbPomTime.ReadOnly = true;
             this.TbPomTime.Size = new System.Drawing.Size(100, 23);
             this.TbPomTime.TabIndex = 4;
             this.TbPomTime.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
-            // 
-            // LblBreakTimer
-            // 
-            this.LblBreakTimer.AutoSize = true;
-            this.LblBreakTimer.Location = new System.Drawing.Point(90, 145);
-            this.LblBreakTimer.Name = "LblBreakTimer";
-            this.LblBreakTimer.Size = new System.Drawing.Size(69, 15);
-            this.LblBreakTimer.TabIndex = 5;
-            this.LblBreakTimer.Text = "Break Timer";
-            // 
-            // LblPomTimer
-            // 
-            this.LblPomTimer.AutoSize = true;
-            this.LblPomTimer.Location = new System.Drawing.Point(79, 9);
-            this.LblPomTimer.Name = "LblPomTimer";
-            this.LblPomTimer.Size = new System.Drawing.Size(96, 15);
-            this.LblPomTimer.TabIndex = 5;
-            this.LblPomTimer.Text = "Pomadoro Timer";
             // 
             // PomadoroForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(248, 227);
-            this.Controls.Add(this.LblPomTimer);
-            this.Controls.Add(this.LblBreakTimer);
             this.Controls.Add(this.TbPomTime);
-            this.Controls.Add(this.BtnPausePom);
             this.Controls.Add(this.BtnStartPom);
             this.Controls.Add(this.TbBreakTime);
-            this.Controls.Add(this.BtnPauseBreak);
             this.Controls.Add(this.BtnStartBreak);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(264, 266);
+            this.MinimumSize = new System.Drawing.Size(264, 266);
             this.Name = "PomadoroForm";
             this.Text = "Pomadoro Timer";
+            this.Load += new System.EventHandler(this.PomadoroForm_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -135,12 +98,8 @@
         #endregion
 
         private System.Windows.Forms.Button BtnStartBreak;
-        private System.Windows.Forms.Button BtnPauseBreak;
         private System.Windows.Forms.TextBox TbBreakTime;
         private System.Windows.Forms.Button BtnStartPom;
-        private System.Windows.Forms.Button BtnPausePom;
-        private System.Windows.Forms.Label LblBreakTimer;
-        private System.Windows.Forms.Label LblPomTimer;
         private System.Windows.Forms.TextBox TbPomTime;
     }
 }
--- PomadoroTimer/PomadoroTimerForm.cs
+++ PomadoroTimer/PomadoroTimerForm.cs
@@ -1,72 +1,84 @@
 ﻿using System;
-using System.Timers;
 using System.Windows.Forms;
-using System.Windows.Forms.
+using System.Media;
+
 
 namespace PomadoroTimer
 {
-
     public partial class PomadoroForm : Form
     {
         public PomadoroForm()
         {
             InitializeComponent();
         }
-        //delegate intialized to enable "safe text" removing exeptions and issues with cross thread callbacks
-        private delegate void SafeCallDelegate(string text);
+  
+        static System.Windows.Forms.Timer pomTimer = new System.Windows.Forms.Timer();
+        static System.Windows.Forms.Timer breakTimer = new System.Windows.Forms.Timer();
 
-        private static System.Timers.Timer pomTimer;
-        private static System.Timers.Timer breakTimer;
-
-        //Method checks for cross threading issues and if required, runs an invoke method using the SafeCallDelegate
-        private void WriteTextSafe(string text)
+        public void EnableButtons() 
         {
-            if (TbPomTime.InvokeRequired)
-            {
-                var d = new SafeCallDelegate(WriteTextSafe);
-                TbPomTime.Invoke(d, new object[] { text });
-            }
-            else
-            {
-                TbPomTime.Text = text;
-            }
+            BtnStartBreak.Enabled = true;
+            BtnStartPom.Enabled = true;
         }
 
-        private void SetPomTimer()
+        public void DisableButtons() 
         {
-            //Construct timer with one second interval
-            pomTimer = new System.Timers.Timer(1000);
-            //hook-up event handler method
-            pomTimer.Elapsed += PomTimerElapsedEvent;
-            pomTimer.AutoReset = true;
-            pomTimer.Enabled = true;
+            BtnStartBreak.Enabled = false;
+            BtnStartPom.Enabled = false;
         }
 
-        private void SetBreakTimer()
+        //Event handler for pomTimer.OnTick
+        private void TimerEventProcessorPom(Object myObject, EventArgs myEventArgs)
         {
-            //Construct timer with one second interval
-            breakTimer = new System.Timers.Timer(1000);
-            //hook-up event handler method
-            breakTimer.Elapsed += BreakTimerElapsedEvent;
-            breakTimer.AutoReset = true;
-            breakTimer.Enabled = true;
+            pomTimer.Stop();
+            //prompt user via system sound and message box
+            SystemSounds.Asterisk.Play();
+            MessageBox.Show("You've completed your Pomadoro", "Pomadoro Completed.", MessageBoxButtons.OK);
+            //Disassociate the event handler created by BtbStartPom_Click
+            //Done in order to make sure user is only prompted once on concurrent pomadoro completions.
+            pomTimer.Tick -= TimerEventProcessorPom;
+            TbPomTime.Text = "Inactive";
+            EnableButtons();
         }
 
-        private void BreakTimerElapsedEvent(object sender, ElapsedEventArgs e)
+        private void TimerEventProcessorBreak(Object myObject, EventArgs myEventArgs)
         {
-
-            TbBreakTime.Text = $"{e.SignalTime}";
+            breakTimer.Stop();
+            //prompt user via system sound and message box
+            SystemSounds.Hand.Play();
+            MessageBox.Show("You've completed your break", "Break Completed.", MessageBoxButtons.OK);
+            //Disassociate the event handler created by BtbStartBreak_Click
+            //Done in order to make sure user is only prompted once on concurrent pomadoro completions.
+            breakTimer.Tick -= TimerEventProcessorBreak;
+            TbBreakTime.Text = "Inactive";
+            EnableButtons();
         }
 
-        private void PomTimerElapsedEvent(object sender, ElapsedEventArgs e)
-        {
-            WriteTextSafe($"{e.SignalTime}");
-        }
-
+        //Start pomadoro, timer is set to 25 minutes
         private void BtnStartPom_Click(object sender, EventArgs e)
         {
-            SetPomTimer();
-            
+            EventHandler handler = new EventHandler(TimerEventProcessorPom);
+            pomTimer.Tick += handler;
+            pomTimer.Interval = 1500000;
+            pomTimer.Start();
+            TbPomTime.Text = "Active";
+            DisableButtons();
+        }
+        //Start break, timer is set to 5 minutes
+        private void BtnStartBreak_Click(object sender, EventArgs e)
+        {
+            EventHandler handler = new EventHandler(TimerEventProcessorBreak);
+            breakTimer.Tick += handler;
+            breakTimer.Interval = 300000;
+            breakTimer.Start();
+            TbBreakTime.Text = "Active";
+            DisableButtons();
+        }
+        //On load set text of TB
+        private void PomadoroForm_Load(object sender, EventArgs e)
+        {
+            TbPomTime.Text = "Inactive";
+            TbBreakTime.Text = "Inactive";
         }
     }
 }
